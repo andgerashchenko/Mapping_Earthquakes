08175402c933dff8014905b04a004cb979fa955f
andgerashchenko <79814533+andgerashchenko@users.noreply.github.com>
2021-07-15 21:26:24 -0400

    // Then we add a control to the map that will allow the user to change // which layers are visible. L.control.layers(baseMaps, overlays).addTo(map); git add .'
    '

--- Earthquakes_past7days/static/js/logicStep4.js
+++ Earthquakes_past7days/static/js/logicStep4.js
@@ -18,6 +18,7 @@ let baseMaps = {
   "Satellite": satelliteStreets
 };
 
+
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
   center: [39.5, -98.5],
@@ -25,9 +26,19 @@ let map = L.map('mapid', {
   layers: [streets]
 })
 
-// Pass our map layers into our layers control and add the layers control to the map.
-L.control.layers(baseMaps).addTo(map);
 
+// Create the earthquake layer for our map.
+let earthquakes = new L.layerGroup();
+
+// We define an object that contains the overlays.
+// This overlay will be visible all the time.
+let overlays = {
+  Earthquakes: earthquakes
+};
+
+// Then we add a control to the map that will allow the user to change
+// which layers are visible.
+L.control.layers(baseMaps, overlays).addTo(map);
 
 // Accessing the airport GeoJSON URL
 let torontoData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
@@ -98,5 +109,6 @@ d3.json(torontoData).then(function(data) {
       onEachFeature: function(feature, layer) {
       layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
     }
-    }).addTo(map);
+    }).addTo(earthquakes);
+    earthquakes.addTo(map);
 });
